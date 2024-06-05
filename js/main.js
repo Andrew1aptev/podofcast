be3a35851b181417c47e2c9f4b85662ef9f2b194
Andey Laptev <laptewandrew@gmail.com>
2024-06-05 15:35:10 +0300

    finished hero section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  centeredSlides: true,
+  slidesPerView: "auto",
+  slidesPerGroup: 2,
+  spaceBetween: 20,
+  initialSlide: 2,
+});
