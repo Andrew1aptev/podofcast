f4d9099e9d4efb52602a8c0b1af1a2e0809e80b0
Andey Laptev <laptewandrew@gmail.com>
2024-06-18 16:43:24 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function setupTabs(buttonClass, gridClass) {
 // Инициализация вкладок для .episodes__button и .episodes__grid
 setupTabs('.episodes__button', '.episodes__grid');
 // Инициализация вкладок для .news__button и .news__grid
-setupTabs('.news__button', '.news__grid');
+setupTabs('.latest-posts__button', '.latest-posts__grid');
 
 
 
